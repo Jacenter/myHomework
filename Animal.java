c358ff383cd6066db4cda309c6dee23dd6a11334
Jacenter <JNovack1501@gmail.com>
2021-03-10 16:48:50 -0500

    This is my animal.java file

---
+++ Animal.java
@@ -0,0 +1,15 @@
+package demos;
+
+public class Animal {
+    public int numberofLegs;
+
+    public Animal(int numberofLegs) {
+        this.numberofLegs = numberofLegs;
+    }
+
+    //Method for animal to walk
+    public void walk() {
+        System.out.println("Look that animal walked");
+    }
+}
+//C:\Users\Jared\IdeaProjects\homeworkforJava\src\main\java\demos\Animal.java
