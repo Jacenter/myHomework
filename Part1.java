0f7be534057086078ea8fe70d172b14ca1bfdd5e
Jacenter <JNovack1501@gmail.com>
2021-03-10 16:50:07 -0500

    this is Part1.java

---
+++ Part1.java
@@ -0,0 +1,11 @@
+package demos;
+
+public class Part1 {
+    public static void main(String[] args) {
+        Animal dog = new Animal(4);
+        System.out.println(dog);
+        dog.walk();
+    }
+}
+
+//C:\Users\Jared\IdeaProjects\homeworkforJava\src\main\java\demos\Part1.java
